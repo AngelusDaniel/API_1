146e3bf955294dba4f5b20aff024c9549432510c
Daniel Angelus <danielangelus@yahoo.com.br>
2020-12-14 23:47:28 -0300

    commit heroku

---
+++ app/Controllers/Http/CursoController.js
@@ -0,0 +1,74 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+const Curso = use("App/Models/Curso")
+
+/**
+ * Resourceful controller for interacting with cursos
+ */
+class CursoController {
+  /**
+   * Show a list of all cursos.
+   * GET cursos
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+    const cursos = await Curso.all();
+    return cursos;
+  }
+
+
+  /**
+   * Create/save a new curso.
+   * POST cursos
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+  }
+
+  /**
+   * Display a single curso.
+   * GET cursos/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+
+  /**
+   * Update curso details.
+   * PUT or PATCH cursos/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a curso with id.
+   * DELETE cursos/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = CursoController
